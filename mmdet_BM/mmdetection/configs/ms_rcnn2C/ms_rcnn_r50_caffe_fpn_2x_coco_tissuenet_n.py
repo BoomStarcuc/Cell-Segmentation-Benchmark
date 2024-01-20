69ea3702b1fa312e4763e9f09e6161fa30e602be
BoomStar <73805598+BoomStarcuc@users.noreply.github.com>
2024-01-20 02:45:50 -0500

    Add files via upload

---
+++ mmdet_BM/mmdetection/configs/ms_rcnn2C/ms_rcnn_r50_caffe_fpn_2x_coco_tissuenet_n.py
@@ -0,0 +1,61 @@
+# _base_ = [
+#     '../_base_/models/mask_rcnn_r50_fpn.py',
+#     '../_base_/datasets/coco_detection.py',
+#     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime_interval1.py', 
+# ]
+_base_ = './ms_rcnn_r50_caffe_fpn_1x_coco_tissuenet_n.py'
+
+img_norm_cfg = dict(
+    mean=[55.0704424, 55.0704424], std=[67.22934807, 67.22934807], to_rgb=False)
+train_pipeline = [
+    dict(type='LoadTIFImageFromFile2C'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='Resize', img_scale=(256, 256), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+test_pipeline = [
+    dict(type='LoadTIFImageFromFile2C'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(256, 256),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+dataset_type = 'CocoTissuenetDataset'
+data_root = 'path/to/your/data/dir'
+data = dict(
+    _delete_=True,
+    samples_per_gpu=48,
+    workers_per_gpu=1,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'tissuenet_nuclear_all_train_2C.json',
+        img_prefix=data_root +'train',
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'tissuenet_nuclear_all_val_2C.json',
+        img_prefix=data_root + 'val',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'tissuenet_nuclear_all_test_2C.json',
+        img_prefix=data_root + 'test',
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric=['bbox'])
+
+# learning policy
+lr_config = dict(step=[16, 22])
+runner = dict(type='EpochBasedRunner', max_epochs=50)
